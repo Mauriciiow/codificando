408047d2b5769729ffa0326cc2f861299d55f785
Mauriciiow <mauricioliveira639@gmail.com>
2021-12-03 03:33:36 -0300

    melhorei o codigo

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
-// var selecionar = document.querySelectorAll('#escolherModo [name=modo]')
-
-// for (let i = 0; i < selecionar.length; i++) {
-//         if (selecionar[i].checked == true) {
-//             console.log('salve');
-//         } else {
-//             console.log('oi');
-//         }
-
-// }
-
 var cesar = document.getElementById("cesar");
 var base = document.getElementById("base");
-var textoChave = document.getElementById("textoChave");
 var divCifra = document.getElementById("divCifra");
 var chave = document.getElementById("chave");
 var valorChave = chave.value;
 var sumir = document.querySelectorAll(".sumir");
+var selecionar = document.getElementById("forma-decifrar");
 
-cesar.addEventListener("click", () => {
-  for (let i = 0; i < sumir.length; i++) {
-    sumir[i].style.display = "flex";
-  }
-
-  divCifra.style.flexDirection = "column";
-});
-
-base.addEventListener("click", () => {
-  for (let i = 0; i < sumir.length; i++) {
-    sumir[i].style.display = "none";
+selecionar.addEventListener("change", () => {
+  for (var i = 0; i < sumir.length; i++) {
+    if (selecionar.value == "CifraCesar") {
+      sumir[i].style.display = "flex";
+      divCifra.style.flexDirection = "column";
+    } else {
+      sumir[i].style.display = "none";
+    }
   }
 });
