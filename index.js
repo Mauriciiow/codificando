5f8e990b3454163b34323a8c5dc32671b2f9e9be
Mauriciiow <mauricioliveira639@gmail.com>
2021-12-03 22:51:37 -0300

    fiz o base64

--- index.js
+++ index.js
@@ -1,18 +1,69 @@
-var cesar = document.getElementById("cesar");
-var base = document.getElementById("base");
-var divCifra = document.getElementById("divCifra");
-var chave = document.getElementById("chave");
-var valorChave = chave.value;
-var sumir = document.querySelectorAll(".sumir");
+var chave = document.getElementById("chave")
 var selecionar = document.getElementById("forma-decifrar");
 
+var divCesar = document.getElementById('escolherCesar')
+var divBase = document.getElementById('escolherBase64')
+var textoCifra = document.getElementById('textoCifra')
+var resultadoCifra = document.getElementById('resultadoCifra')
+var codificarCesar = document.getElementById('codificarCesar')
+var decodificarCesar = document.getElementById('decodificarCesar')
+
+var textoBase = document.getElementById('textoBase')
+var resultadoBase = document.getElementById('resultadoBase')
+var codificarBase = document.getElementById('codificarBase')
+var decodificarBase = document.getElementById('decodificarBase')
+
+var esconderCesar = function () {
+  divCesar.style.display = 'none'
+  divBase.style.display = 'flex'
+}
+
+var esconderBase = function () {
+  divBase.style.display = 'none'
+  divCesar.style.display = 'flex'
+}
+
 selecionar.addEventListener("change", () => {
-  for (var i = 0; i < sumir.length; i++) {
-    if (selecionar.value == "CifraCesar") {
-      sumir[i].style.display = "flex";
-      divCifra.style.flexDirection = "column";
-    } else {
-      sumir[i].style.display = "none";
-    }
+  if (selecionar.value == 'Base64') {
+    esconderCesar()
+  } else {
+    esconderBase()
   }
 });
+
+window.onload = function () {
+  divCesar.style.display = 'none'
+  divBase.style.display = 'none'
+}
+
+
+
+var converterBase = function(texto) {
+  var resb64 = btoa(texto)
+  return resb64
+}
+var decodificaBase = function (texto) {
+  var decb64 = atob(texto)
+  return decb64
+}
+
+textoBase.addEventListener('keyup', ()=>{
+  var textoBaseValor = textoBase.value
+  var codChecked =  codificarBase.checked
+  var decodChecked = decodificarBase.checked
+  if (codChecked) {
+    resultadoBase.value = converterBase(textoBaseValor)
+  } else if (decodChecked) {
+    resultadoBase.value = decodificaBase(textoBaseValor)
+  } 
+  
+
+})
+
+textoCifra.addEventListener('keyup', ()=>{
+  var chaveValor = chave.value
+  var textoCifraValor = textoCifra.value
+  var codificarChecked = codificarCesar.checked
+  var decodificarChecked = decodificarCesar.checked
+  
+})
